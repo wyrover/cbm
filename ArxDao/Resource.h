54d3886c4618f79a5523ce9730b01ff7f4bc8bbd
dlj <anheihb03dlj@163.com>
2015-07-22 18:13:48 +0800

    1、尝试在arxdao项目中使用duivision库（原封不动的使用它自带的demo），失败了 原因：   在arx中使用duivision，create对话框总是失败，提示内存冲突，原因实在是查不出来
    2、做了一个简单的DuiSystemManager封装类，简化代码，用法请参见DuiVisionDemo.cpp

--- ArxDao/Resource.h
+++ ArxDao/Resource.h
@@ -2,15 +2,15 @@
 // Microsoft Visual C++ generated include file.
 // Used by ArxDao.rc
 //
-#define IDS_PROJNAME 100
+#define IDS_PROJNAME                    100
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         32768
-#define _APS_NEXT_CONTROL_VALUE         100
+#define _APS_NEXT_CONTROL_VALUE         101
 #define _APS_NEXT_SYMED_VALUE           102
 #endif
 #endif
