f717a1f33b5ab6e886a15e2968c980d0fee2ea7e
dlj <anheihb03dlj@163.com>
2015-08-12 17:41:28 +0800

    删除不再需要的第3方库: sar(stactiverecord）、storm

--- ThirdParty/stactiverecord/include/config.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define HAVE_SQLITE3
-#define HAVE_MYSQL
-//#define HAVE_POSTGRESQL
--- ThirdParty/stactiverecord/tests/run_tests.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-(./db_test $1 && ./object_test $1 && echo "Tests completed successfully") || echo "Tests failed"
