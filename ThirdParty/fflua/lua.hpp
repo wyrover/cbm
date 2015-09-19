d8787105a4d4dc1c1d7cd21a1e1f5a42ff12c8a1
dlj <anheihb03dlj@163.com>
2015-09-20 00:34:09 +0800

    增加一个lua c++的绑定库，后续可能会用于脚本设计(maybe ^_^)

---
+++ ThirdParty/fflua/lua.hpp
@@ -0,0 +1,14 @@
+#ifndef _LUA_HPP
+#define _LUA_HPP
+
+// lua.hpp
+// Lua header files for C++
+// <<extern "C">> not supplied automatically because Lua also compiles as C++
+
+extern "C" {
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
+
+#endif _LUA_HPP
