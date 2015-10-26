d4257d71bdddf8e5d808072dde4cb6028ddef319
dlj <anheihb03dlj@163.com>
2015-10-26 19:27:29 +0800

    修复thrift的编译错误(int64_t等typedef符号冲突的错误)

--- ThirdParty/thrift/lib/cpp/src/thrift/protocol/TDenseProtocol.cpp
+++ ThirdParty/thrift/lib/cpp/src/thrift/protocol/TDenseProtocol.cpp
@@ -91,7 +91,7 @@ absent and prefix them with an 0x01 byte if they are present
 
 #include <thrift/thrift-config.h>
 
-#include <stdint.h>
+//#include <stdint.h>
 #include <thrift/protocol/TDenseProtocol.h>
 #include <thrift/TReflectionLocal.h>
 
--- ThirdParty/thrift/lib/cpp/src/thrift/windows/config.h
+++ ThirdParty/thrift/lib/cpp/src/thrift/windows/config.h
@@ -61,17 +61,141 @@
 
 #ifdef HAVE_STDINT_H
 #  include <stdint.h>
+
+using std::int64_t;
+using std::uint64_t;
+using std::int32_t;
+using std::uint32_t;
+using std::int16_t;
+using std::uint16_t;
+using std::int8_t;
+using std::uint8_t;
+
+//typedef std::int64_t    int64_t;
+//typedef std::uint64_t  uint64_t;
+//typedef std::int32_t    int32_t;
+//typedef std::uint32_t  uint32_t;
+//typedef std::int16_t    int16_t;
+//typedef std::uint16_t  uint16_t;
+//typedef std::int8_t      int8_t;
+//typedef std::uint8_t    uint8_t;
+
 #else
 #  include <boost/cstdint.hpp>
 
-typedef boost::int64_t    int64_t;
-typedef boost::uint64_t  uint64_t;
-typedef boost::int32_t    int32_t;
-typedef boost::uint32_t  uint32_t;
-typedef boost::int16_t    int16_t;
-typedef boost::uint16_t  uint16_t;
-typedef boost::int8_t      int8_t;
-typedef boost::uint8_t    uint8_t;
+//改成使用using
+using boost::int64_t;
+using boost::uint64_t;
+using boost::int32_t;
+using boost::uint32_t;
+using boost::int16_t;
+using boost::uint16_t;
+using boost::int8_t;
+using boost::uint8_t;
+
+#if !defined(__cplusplus) || defined(__STDC_LIMIT_MACROS) // [   See footnote 220 at page 257 and footnote 221 at page 259
+
+// 7.18.2.1 Limits of exact-width integer types
+#define INT8_MIN     ((int8_t)_I8_MIN)
+#define INT8_MAX     _I8_MAX
+#define INT16_MIN    ((int16_t)_I16_MIN)
+#define INT16_MAX    _I16_MAX
+#define INT32_MIN    ((int32_t)_I32_MIN)
+#define INT32_MAX    _I32_MAX
+#define INT64_MIN    ((int64_t)_I64_MIN)
+#define INT64_MAX    _I64_MAX
+#define UINT8_MAX    _UI8_MAX
+#define UINT16_MAX   _UI16_MAX
+#define UINT32_MAX   _UI32_MAX
+#define UINT64_MAX   _UI64_MAX
+
+// 7.18.2.2 Limits of minimum-width integer types
+#define INT_LEAST8_MIN    INT8_MIN
+#define INT_LEAST8_MAX    INT8_MAX
+#define INT_LEAST16_MIN   INT16_MIN
+#define INT_LEAST16_MAX   INT16_MAX
+#define INT_LEAST32_MIN   INT32_MIN
+#define INT_LEAST32_MAX   INT32_MAX
+#define INT_LEAST64_MIN   INT64_MIN
+#define INT_LEAST64_MAX   INT64_MAX
+#define UINT_LEAST8_MAX   UINT8_MAX
+#define UINT_LEAST16_MAX  UINT16_MAX
+#define UINT_LEAST32_MAX  UINT32_MAX
+#define UINT_LEAST64_MAX  UINT64_MAX
+
+// 7.18.2.3 Limits of fastest minimum-width integer types
+#define INT_FAST8_MIN    INT8_MIN
+#define INT_FAST8_MAX    INT8_MAX
+#define INT_FAST16_MIN   INT16_MIN
+#define INT_FAST16_MAX   INT16_MAX
+#define INT_FAST32_MIN   INT32_MIN
+#define INT_FAST32_MAX   INT32_MAX
+#define INT_FAST64_MIN   INT64_MIN
+#define INT_FAST64_MAX   INT64_MAX
+#define UINT_FAST8_MAX   UINT8_MAX
+#define UINT_FAST16_MAX  UINT16_MAX
+#define UINT_FAST32_MAX  UINT32_MAX
+#define UINT_FAST64_MAX  UINT64_MAX
+
+// 7.18.2.4 Limits of integer types capable of holding object pointers
+#ifdef _WIN64 // [
+#  define INTPTR_MIN   INT64_MIN
+#  define INTPTR_MAX   INT64_MAX
+#  define UINTPTR_MAX  UINT64_MAX
+#else // _WIN64 ][
+#  define INTPTR_MIN   INT32_MIN
+#  define INTPTR_MAX   INT32_MAX
+#  define UINTPTR_MAX  UINT32_MAX
+#endif // _WIN64 ]
+
+// 7.18.2.5 Limits of greatest-width integer types
+#define INTMAX_MIN   INT64_MIN
+#define INTMAX_MAX   INT64_MAX
+#define UINTMAX_MAX  UINT64_MAX
+
+// 7.18.3 Limits of other integer types
+
+#ifdef _WIN64 // [
+#  define PTRDIFF_MIN  _I64_MIN
+#  define PTRDIFF_MAX  _I64_MAX
+#else  // _WIN64 ][
+#  define PTRDIFF_MIN  _I32_MIN
+#  define PTRDIFF_MAX  _I32_MAX
+#endif  // _WIN64 ]
+
+#define SIG_ATOMIC_MIN  INT_MIN
+#define SIG_ATOMIC_MAX  INT_MAX
+
+#ifndef SIZE_MAX // [
+#  ifdef _WIN64 // [
+#     define SIZE_MAX  _UI64_MAX
+#  else // _WIN64 ][
+#     define SIZE_MAX  _UI32_MAX
+#  endif // _WIN64 ]
+#endif // SIZE_MAX ]
+
+// WCHAR_MIN and WCHAR_MAX are also defined in <wchar.h>
+#ifndef WCHAR_MIN // [
+#  define WCHAR_MIN  0
+#endif  // WCHAR_MIN ]
+#ifndef WCHAR_MAX // [
+#  define WCHAR_MAX  _UI16_MAX
+#endif  // WCHAR_MAX ]
+
+#define WINT_MIN  0
+#define WINT_MAX  _UI16_MAX
+
+#endif // __STDC_LIMIT_MACROS ]
+
+//vc2008编译器并不能正确识别这种语法
+//typedef boost::int64_t    int64_t;
+//typedef boost::uint64_t  uint64_t;
+//typedef boost::int32_t    int32_t;
+//typedef boost::uint32_t  uint32_t;
+//typedef boost::int16_t    int16_t;
+//typedef boost::uint16_t  uint16_t;
+//typedef boost::int8_t      int8_t;
+//typedef boost::uint8_t    uint8_t;
 #endif
 
 #include <thrift/transport/PlatformSocket.h>
