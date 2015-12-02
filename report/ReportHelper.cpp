f6cbfa91b6ca0dad519043fb6d8e33bbb3ad926a
dlj <anheihb03dlj@163.com>
2015-12-02 23:45:08 +0800

    清理不在需要的代码和项目

--- report/ReportHelper.cpp
+++
@@ -1,8 +0,0 @@
-#include "stdafx.h"
-#include "ReportHelper.h"
-
-bool ReportHelper::CreatReport(CString& jsonPath)
-{
-	WordHelper wdHelper;
-	return wdHelper.creat(jsonPath);
-}
--- report/ReportHelper.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "WordHelper.h"
-
-class ReportHelper
-{
-public:
-	static bool CreatReport(CString& jsonPath);
-};
